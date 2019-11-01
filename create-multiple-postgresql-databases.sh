24cc8c59b913376c1ea6e8aee56ee43087d81d7c
Brian Tai <btai@soxhub.com>
2019-11-01 13:46:51 -0700

    add some performance enhancements to postgres test db

--- create-multiple-postgresql-databases.sh
+++ create-multiple-postgresql-databases.sh
@@ -13,7 +13,20 @@ function create_user_and_database() {
 EOSQL
 }
 
+function improve_postgres_performance() {
+	echo "Configuring psql with improved performance..."
+
+	sed -ri "s/^#*(fsync\s*=\s*)\S+/\1 off/" "$PGDATA"/postgresql.conf
+	sed -ri "s/^#*(full_page_writes\s*=\s*)\S+/\1 off/" "$PGDATA"/postgresql.conf
+	sed -ri "s/^#*(random_page_cost\s*=\s*)\S+/\1 2.0/" "$PGDATA"/postgresql.conf
+	sed -ri "s/^#*(checkpoint_segments\s*=\s*)\S+/\1 64/" "$PGDATA"/postgresql.conf
+	sed -ri "s/^#*(checkpoint_completion_target\s*=\s*)\S+/\1 0.9/" "$PGDATA"/postgresql.conf
+
+	grep fsync "$PGDATA"/postgresql.conf
+}
+
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	improve_postgres_performance
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
